9ba1270173632a9b7edfd23bf5b36204d2e6665c
Pavel-Burov <the.pashasha@gmail.com>
2024-09-20 10:45:59 +0300

    add struct game account

--- programs/tic-tac-toe/src/lib.rs
+++ programs/tic-tac-toe/src/lib.rs
@@ -14,3 +14,11 @@ pub mod tic_tac_toe {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Game {
+    players: [Pubkey; 2],          // (32 * 2)
+    turn: u8,                      // 1
+    board: [[Option<Sign>; 3]; 3], // 9 * (1 + 1) = 18
+    state: GameState,              // 32 + 1
+}
